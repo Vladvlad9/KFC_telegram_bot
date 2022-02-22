c9a37c0aefd8a78979a31030b36f0a6353590fdf
Vlad Paulechka <vlad9.ru@gmail.com>
2022-02-22 14:12:00 +0300

    add new update
    add new update for function def sql_update_action_user_restaraunt

--- keyboards/inline/user/restaurants.py
+++ keyboards/inline/user/restaurants.py
@@ -60,23 +60,3 @@ async def change_product_keyboard_inline():
 
 
 
-"""Купоны"""
-async def coupons_inline():
-    keyboard = InlineKeyboardMarkup(
-        row_width=1
-    )
-    keyboard.add(*[InlineKeyboardButton(text="Просмотреть", callback_data=f"coupons")])
-    return keyboard
-
-
-async def get_coupons_keyboard_inline(name_products, price):
-    buttons = [
-        types.InlineKeyboardButton(text="-1", callback_data=f'coup_decr_{name_products}_{price}'),
-        types.InlineKeyboardButton(text="+1", callback_data=f'coup_incr_{name_products}_{price}'),
-        #types.InlineKeyboardButton(text="Изменить состав", callback_data=f'change_{name_products}'),
-        InlineKeyboardButton(text="Добавить в корзину", callback_data=f"addCoup_{name_products}_{price}"),
-    ]
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
-    keyboard.add(*buttons)
-
-    return keyboard
