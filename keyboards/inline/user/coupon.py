3687fa39984ef664a4e8e670de5cc141572788d4
Vlad Paulechka <vlad9.ru@gmail.com>
2022-02-22 15:03:16 +0300

    add new inline
    add new inline keyboard

--- keyboards/inline/user/coupon.py
+++ keyboards/inline/user/coupon.py
@@ -2,11 +2,19 @@ from aiogram import types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-async def get_coupons_keyboard_inline(price, name_сoupons):
+async def coupons_inline():
+    keyboard = InlineKeyboardMarkup(
+        row_width=1
+    )
+    keyboard.add(*[InlineKeyboardButton(text="Просмотреть", callback_data=f"coupons")])
+    return keyboard
+
+
+async def get_coupons_keyboard_inline(name_products, price):
     buttons = [
-        types.InlineKeyboardButton(text="-1", callback_data=f'num_decr_{name_сoupons}_{price}'),
-        types.InlineKeyboardButton(text="+1", callback_data=f'num_incr_{name_сoupons}_{price}'),
-        InlineKeyboardButton(text="Добавить в корзину", callback_data=f"add_{name_сoupons}_{price}"),
+        types.InlineKeyboardButton(text="-1", callback_data=f'coup_decr_{name_products}_{price}'),
+        types.InlineKeyboardButton(text="+1", callback_data=f'coup_incr_{name_products}_{price}'),
+        InlineKeyboardButton(text="Добавить в корзину", callback_data=f"addCoup_{name_products}_{price}"),
     ]
     keyboard = types.InlineKeyboardMarkup(row_width=2)
     keyboard.add(*buttons)
